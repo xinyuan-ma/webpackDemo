db6cce550a9340660062d436a84d5b59e05ddf4c
BWH\Chris.Ma <chris.ma@visitshanghai.net>
2020-06-23 13:58:03 +0800

    增加loader 的不同引入方式

--- webpack.config.js
+++ webpack.config.js
@@ -6,16 +6,36 @@ module.exports = {
     path: path.join(__dirname, "distLoader"),
     filename: "bundle.js"
   },
+  /**
+   * loader默认会在node_modules查找，也可以指定其他的目录，来查找我们自定义的loader
+   * */
+  resolveLoader: {
+    modules: ['node_modules', path.resolve(__dirname, 'loader', 'loader')]
+  },
   module: {
     rules: [
       {
         test: /\.demo$/,
-        use: [
-          path.resolve(__dirname, 'loader/loader', 'style-loader'),
-          path.resolve(__dirname, 'loader/loader', 'demo-loader')
-        ]
+        use: [  'style-loader', 'demo-loader']
       }]
-  }
+  },
+
+  /**
+   * loader使用绝对路径的方式引入
+   * */
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.demo$/,
+  //       use: [
+  //         path.resolve(__dirname, 'loader/loader', 'style-loader'),
+  //         path.resolve(__dirname, 'loader/loader', 'demo-loader')
+  //       ]
+  //     }]
+  // }
+
+
+
   // resolveLoader: {
   //   modules: [path.join(__dirname, "./src/loaders"), "node_modules"]
   // },
